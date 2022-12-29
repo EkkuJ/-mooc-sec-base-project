7d0f3adb244a83f77c059e0c6fc62067a83198c0
Ekku Jokinen <ekku@inven.ai>
2022-12-29 18:20:38 +0200

    Add a download view

--- projectsite/projectsite/pages/views.py
+++ projectsite/projectsite/pages/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 from .models import Todo
+
 
 @login_required
 @csrf_exempt
@@ -14,15 +16,27 @@ def indexView(request):
 @login_required
 @csrf_exempt
 def addView(request):
-	content = request.POST.get('content')
-	if len(content) > 0:
-		todo = Todo(owner=request.user, content=content)
-		todo.save()
-	return redirect('/')
+    content = request.POST.get('content')
+    if len(content) > 0:
+        todo = Todo(owner=request.user, content=content)
+        todo.save()
+    return redirect('/')
+
 
 @login_required
 @csrf_exempt
 def deleteView(request):
-	todo = Todo.objects.get(id=request.POST.get('id'))
-	todo.delete()
-	return redirect('/')
+    todo = Todo.objects.get(id=request.POST.get('id'))
+    todo.delete()
+    return redirect('/')
+
+
+@login_required
+@csrf_exempt
+def downloadView(request):
+    user = request.GET.get('user')
+    todos = []
+    set = Todo.objects.filter(owner__username=user)
+    for todo in set:
+        todos.append(todo.content)
+    return JsonResponse({'todos': todos})
