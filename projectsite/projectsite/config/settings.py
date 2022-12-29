fc74c9fe5a40abcd111c2ddec6cb4a30029feb39
Ekku Jokinen <ekku@inven.ai>
2022-12-29 20:22:00 +0200

    Add correct psw validators as comment

--- projectsite/projectsite/config/settings.py
+++ projectsite/projectsite/config/settings.py
@@ -91,6 +91,21 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = []
 
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
