c5caa65acfdd080c4891d0561d113d863e945097
Ekku Jokinen <ekku@inven.ai>
2022-12-29 20:05:32 +0200

    Add signup and auth links

--- projectsite/projectsite/pages/urls.py
+++ projectsite/projectsite/pages/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import indexView, addView, deleteView, downloadView
+from .views import indexView, addView, deleteView, downloadView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='pages/login.html')),
     path('logout/', LogoutView.as_view(next_page='/')),
+    path('signup/', SignUpView, name='signup'),
     path('', indexView),
     path('add/', addView, name='add'),
     path('delete/', deleteView, name='delete'),
--- projectsite/projectsite/pages/views.py
+++ projectsite/projectsite/pages/views.py
@@ -1,10 +1,25 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
 from .models import Todo
 
+def SignUpView(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'pages/signup.html', {'form': form})
 
 @login_required
 @csrf_exempt
